b9539d8d825e0ded9d4255795418832c28ef20db
Khanh Ngoc <abcml0207@gmail.com>
2022-10-21 15:26:34 +0700

    first commit

---
+++ Tuan01_20521662.sql
@@ -0,0 +1,325 @@
+﻿CREATE DATABASE QLBH
+USE QLBH
+GO
+-- KHACHANG
+CREATE TABLE KHACHHANG(
+	MAKH	char(4) not null,	
+	HOTEN	varchar(40),
+	DCHI	varchar(50),
+	SODT	varchar(20),
+	NGSINH	smalldatetime,
+	NGDK	smalldatetime,
+	DOANHSO	money,
+	CONSTRAINT pk_kh PRIMARY KEY(MAKH)
+)
+---------------------------------------------
+-- NHANVIEN
+CREATE TABLE NHANVIEN(
+	MANV	char(4) not null,	
+	HOTEN	varchar(40),
+	SODT	varchar(20),
+	NGVL	smalldatetime	
+	constraint pk_nv primary key(MANV)
+)
+---------------------------------------------
+-- SANPHAM
+CREATE TABLE SANPHAM(
+	MASP	char(4) not null,
+	TENSP	varchar(40),
+	DVT	varchar(20),
+	NUOCSX	varchar(40),
+	GIA	money,
+	constraint pk_sp primary key(MASP)	
+)
+---------------------------------------------
+-- HOADON
+CREATE TABLE HOADON(
+	SOHD	int not null,
+	NGHD 	smalldatetime,
+	MAKH 	char(4),
+	MANV 	char(4),
+	TRIGIA	money,
+	constraint pk_hd primary key(SOHD)
+)
+---------------------------------------------
+-- CTHD
+   CREATE TABLE CTHD(
+	SOHD	int,
+	MASP	char(4),
+	SL	int,
+	constraint pk_cthd primary key(SOHD,MASP)
+)
+
+-- Khoa ngoai cho bang HOADON
+ALTER TABLE HOADON ADD CONSTRAINT fk01_HD FOREIGN KEY(MAKH) REFERENCES KHACHHANG(MAKH)
+ALTER TABLE HOADON ADD CONSTRAINT fk02_HD FOREIGN KEY(MANV) REFERENCES NHANVIEN(MANV)
+-- Khoa ngoai cho bang CTHD
+ALTER TABLE CTHD ADD CONSTRAINT fk01_CTHD FOREIGN KEY(SOHD) REFERENCES HOADON(SOHD)
+ALTER TABLE CTHD ADD CONSTRAINT fk02_CTHD FOREIGN KEY(MASP) REFERENCES SANPHAM(MASP)
+-----------------------------------------------------
+-----------------------------------------------------
+set dateformat dmy
+-------------------------------
+-- KHACHHANG
+insert into khachhang values('KH01','Nguyen Van A','731 Tran Hung Dao, Q5, TpHCM','8823451','22/10/1960','22/07/2006',13060000)
+insert into khachhang values('KH02','Tran Ngoc Han','23/5 Nguyen Trai, Q5, TpHCM','908256478','03/04/1974','30/07/2006',280000)
+insert into khachhang values('KH03','Tran Ngoc Linh','45 Nguyen Canh Chan, Q1, TpHCM','938776266','12/06/1980','08/05/2006',3860000)
+insert into khachhang values('KH04','Tran Minh Long','50/34 Le Dai Hanh, Q10, TpHCM','917325476','09/03/1965','10/02/2006',250000)
+insert into khachhang values('KH05','Le Nhat Minh','34 Truong Dinh, Q3, TpHCM','8246108','10/03/1950','28/10/2006',21000)
+insert into khachhang values('KH06','Le Hoai Thuong','227 Nguyen Van Cu, Q5, TpHCM','8631738','31/12/1981','24/11/2006',915000)
+insert into khachhang values('KH07','Nguyen Van Tam','32/3 Tran Binh Trong, Q5, TpHCM','916783565','06/04/1971','12/01/2006',12500)
+insert into khachhang values('KH08','Phan Thi Thanh','45/2 An Duong Vuong, Q5, TpHCM','938435756','10/01/1971','13/12/2006',365000)
+insert into khachhang values('KH09','Le Ha Vinh','873 Le Hong Phong, Q5, TpHCM','8654763','03/09/1979','14/01/2007',70000)
+insert into khachhang values('KH10','Ha Duy Lap','34/34B Nguyen Trai, Q1, TpHCM','8768904','02/05/1983','16/01/2007',67500)
+
+-------------------------------
+-- NHANVIEN
+insert into nhanvien values('NV01','Nguyen Nhu Nhut','927345678','13/04/2006')
+insert into nhanvien values('NV02','Le Thi Phi Yen','987567390','21/04/2006')
+insert into nhanvien values('NV03','Nguyen Van B','997047382','27/04/2006')
+insert into nhanvien values('NV04','Ngo Thanh Tuan','913758498','24/06/2006')
+insert into nhanvien values('NV05','Nguyen Thi Truc Thanh','918590387','20/07/2006')
+
+-------------------------------
+-- SANPHAM
+insert into sanpham values('BC01','But chi','cay','Singapore',3000)
+insert into sanpham values('BC02','But chi','cay','Singapore',5000)
+insert into sanpham values('BC03','But chi','cay','Viet Nam',3500)
+insert into sanpham values('BC04','But chi','hop','Viet Nam',30000)
+insert into sanpham values('BB01','But bi','cay','Viet Nam',5000)
+insert into sanpham values('BB02','But bi','cay','Trung Quoc',7000)
+insert into sanpham values('BB03','But bi','hop','Thai Lan',100000)
+insert into sanpham values('TV01','Tap 100 giay mong','quyen','Trung Quoc',2500)
+insert into sanpham values('TV02','Tap 200 giay mong','quyen','Trung Quoc',4500)
+insert into sanpham values('TV03','Tap 100 giay tot','quyen','Viet Nam',3000)
+insert into sanpham values('TV04','Tap 200 giay tot','quyen','Viet Nam',5500)
+insert into sanpham values('TV05','Tap 100 trang','chuc','Viet Nam',23000)
+insert into sanpham values('TV06','Tap 200 trang','chuc','Viet Nam',53000)
+insert into sanpham values('TV07','Tap 100 trang','chuc','Trung Quoc',34000)
+insert into sanpham values('ST01','So tay 500 trang','quyen','Trung Quoc',40000)
+insert into sanpham values('ST02','So tay loai 1','quyen','Viet Nam',55000)
+insert into sanpham values('ST03','So tay loai 2','quyen','Viet Nam',51000)
+insert into sanpham values('ST04','So tay','quyen','Thai Lan',55000)
+insert into sanpham values('ST05','So tay mong','quyen','Thai Lan',20000)
+insert into sanpham values('ST06','Phan viet bang','hop','Viet Nam',5000)
+insert into sanpham values('ST07','Phan khong bui','hop','Viet Nam',7000)
+insert into sanpham values('ST08','Bong bang','cai','Viet Nam',1000)
+insert into sanpham values('ST09','But long','cay','Viet Nam',5000)
+insert into sanpham values('ST10','But long','cay','Trung Quoc',7000)
+
+-------------------------------
+-- HOADON
+insert into hoadon values(1001,'23/07/2006','KH01','NV01',320000)
+insert into hoadon values(1002,'12/08/2006','KH01','NV02',840000)
+insert into hoadon values(1003,'23/08/2006','KH02','NV01',100000)
+insert into hoadon values(1004,'01/09/2006','KH02','NV01',180000)
+insert into hoadon values(1005,'20/10/2006','KH01','NV02',3800000)
+insert into hoadon values(1006,'16/10/2006','KH01','NV03',2430000)
+insert into hoadon values(1007,'28/10/2006','KH03','NV03',510000)
+insert into hoadon values(1008,'28/10/2006','KH01','NV03',440000)
+insert into hoadon values(1009,'28/10/2006','KH03','NV04',200000)
+insert into hoadon values(1010,'01/11/2006','KH01','NV01',5200000)
+insert into hoadon values(1011,'04/11/2006','KH04','NV03',250000)
+insert into hoadon values(1012,'30/11/2006','KH05','NV03',21000)
+insert into hoadon values(1013,'12/12/2006','KH06','NV01',5000)
+insert into hoadon values(1014,'31/12/2006','KH03','NV02',3150000)
+insert into hoadon values(1015,'01/01/2007','KH06','NV01',910000)
+insert into hoadon values(1016,'01/01/2007','KH07','NV02',12500)
+insert into hoadon values(1017,'02/01/2007','KH08','NV03',35000)
+insert into hoadon values(1018,'13/01/2007','KH08','NV03',330000)
+insert into hoadon values(1019,'13/01/2007','KH01','NV03',30000)
+insert into hoadon values(1020,'14/01/2007','KH09','NV04',70000)
+insert into hoadon values(1021,'16/01/2007','KH10','NV03',67500)
+insert into hoadon values(1022,'16/01/2007',Null,'NV03',7000)
+insert into hoadon values(1023,'17/01/2007',Null,'NV01',330000)
+
+-------------------------------
+-- CTHD
+insert into cthd values(1001,'TV02',10)
+insert into cthd values(1001,'ST01',5)
+insert into cthd values(1001,'BC01',5)
+insert into cthd values(1001,'BC02',10)
+insert into cthd values(1001,'ST08',10)
+insert into cthd values(1002,'BC04',20)
+insert into cthd values(1002,'BB01',20)
+insert into cthd values(1002,'BB02',20)
+insert into cthd values(1003,'BB03',10)
+insert into cthd values(1004,'TV01',20)
+insert into cthd values(1004,'TV02',10)
+insert into cthd values(1004,'TV03',10)
+insert into cthd values(1004,'TV04',10)
+insert into cthd values(1005,'TV05',50)
+insert into cthd values(1005,'TV06',50)
+insert into cthd values(1006,'TV07',20)
+insert into cthd values(1006,'ST01',30)
+insert into cthd values(1006,'ST02',10)
+insert into cthd values(1007,'ST03',10)
+insert into cthd values(1008,'ST04',8)
+insert into cthd values(1009,'ST05',10)
+insert into cthd values(1010,'TV07',50)
+insert into cthd values(1010,'ST07',50)
+insert into cthd values(1010,'ST08',100)
+insert into cthd values(1010,'ST04',50)
+insert into cthd values(1010,'TV03',100)
+insert into cthd values(1011,'ST06',50)
+insert into cthd values(1012,'ST07',3)
+insert into cthd values(1013,'ST08',5)
+insert into cthd values(1014,'BC02',80)
+insert into cthd values(1014,'BB02',100)
+insert into cthd values(1014,'BC04',60)
+insert into cthd values(1014,'BB01',50)
+insert into cthd values(1015,'BB02',30)
+insert into cthd values(1015,'BB03',7)
+insert into cthd values(1016,'TV01',5)
+insert into cthd values(1017,'TV02',1)
+insert into cthd values(1017,'TV03',1)
+insert into cthd values(1017,'TV04',5)
+insert into cthd values(1018,'ST04',6)
+insert into cthd values(1019,'ST05',1)
+insert into cthd values(1019,'ST06',2)
+insert into cthd values(1020,'ST07',10)
+insert into cthd values(1021,'ST08',5)
+insert into cthd values(1021,'TV01',7)
+insert into cthd values(1021,'TV02',10)
+insert into cthd values(1022,'ST07',1)
+insert into cthd values(1023,'ST04',6)
+--I. Ngôn ngữ định nghĩa dữ liệu (Data Definition Language):
+--2. Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
+ALTER TABLE SANPHAM 
+ADD GHICHU VARCHAR(20)
+--3. Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG.
+ALTER TABLE KHACHHANG
+ADD LOAIKH TINYINT 
+--4. Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100).
+ALTER TABLE SANPHAM 
+ALTER COLUMN GHICHU VARCHAR(100)
+--5. Xóa thuộc tính GHICHU trong quan hệ SANPHAM.
+ALTER TABLE SANPHAM 
+DROP COLUMN GHICHU
+--6. Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”, …
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH VARCHAR(50)
+--7. Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)
+ALTER TABLE SANPHAM
+ADD CONSTRAINT DVT CHECK(DVT ='cay' or DVT='hop' or DVT ='cai' or DVT='quyen'or DVT='chuc')
+--8. Giá bán của sản phẩm từ 500 đồng trở lên.
+ALTER TABLE SANPHAM
+ADD CONSTRAINT GIA CHECK(GIA>500)
+-- 9. Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm.
+ALTER TABLE CTHD
+ADD CONSTRAINT SL CHECK(SL>=1)
+--10. Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó.
+ALTER TABLE KHACHHANG
+ADD CONSTRAINT NGDK CHECK(NGDK>=NGSINH)
+-- II. Ngôn ngữ thao tác dữ liệu (Data Manipulation Language):
+--2. Tạo quan hệ SANPHAM1 chứa toàn bộ dữ liệu của quan hệ SANPHAM. Tạo quan hệ KHACHHANG1 chứa toàn bộ dữ liệu của quan hệ KHACHHANG.
+SELECT * INTO SANPHAM1
+FROM SANPHAM
+SELECT*FROM SANPHAM1
+
+SELECT * INTO KHACHHANG1
+FROM KHACHHANG
+SELECT*FROM KHACHHANG1
+
+--3. Cập nhật giá tăng 5% đối với những sản phẩm do “Thai Lan” sản xuất (cho quan hệ SANPHAM1)
+UPDATE SANPHAM1
+SET  GIA=GIA*0.05 
+WHERE NUOCSX='Thai Lan' 
+
+--4. Cập nhật giá giảm 5% đối với những sản phẩm do “Trung Quoc” sản xuất có giá từ 10.000 trở xuống (cho quan hệ SANPHAM1).
+UPDATE SANPHAM1
+SET GIA=GIA/0.05
+WHERE NUOCSX='Trung Quoc' AND GIA<10000 
+
+--5. Cập nhật giá trị LOAIKH là “Vip” đối với những khách hàng đăng ký thành viên trước ngày 1/1/2007 có doanh số từ 10.000.000 trở lên hoặc khách hàng đăng ký thành viên từ 1/1/2007 trở về sau có doanh số từ 2.000.000 trở lên (cho quan hệ KHACHHANG1).
+
+UPDATE KHACHHANG1
+SET LOAIKH='Vip'
+WHERE NGDK<'1/1/2007' AND DOANHSO >10000000 OR  NGDK>'1/1/2007' AND DOANHSO >2000000
+
+
+--III. Ngôn ngữ truy vấn dữ liệu có cấu trúc:
+--1. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP,TENSP
+FROM SANPHAM
+WHERE NUOCSX ='Trung Quoc'
+
+--2. In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE DVT = 'CAY' AND DVT ='QUYEN'
+
+--3. In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+SELECT MASP,TENSP
+FROM SANPHAM
+WHERE MASP LIKE'B%01' --BC01, BB01--
+
+--4. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX ='Trung Quoc' AND GIA > 30000 AND GIA < 40000
+
+--5. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX='Trung Quoc' AND GIA>30000 AND GIA < 40000 or NUOCSX='Thai Lan' AND GIA>30000 AND GIA < 40000
+--6. In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE  NGHD >= '1/1/2007'  AND NGHD<='2/1/2007'
+--7. In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+
+-- Tăng Dần--
+SELECT SOHD,TRIGIA
+FROM HOADON
+WHERE NGHD>='1/1/2007' AND NGHD<='30/1/2007'
+ORDER BY NGHD ASC
+
+-- Giảm dần --
+SELECT SOHD,TRIGIA
+FROM HOADON
+WHERE MONTH(NGHD)=1  AND YEAR(NGHD)=1
+ORDER BY NGHD DESC
+
+--8. In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT  HOTEN
+FROM KHACHHANG
+INNER JOIN HOADON
+ON KHACHHANG.MAKH = HOADON.MAKH
+WHERE NGHD='1/1/2007'
+--9. In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
+SELECT SOHD, TRIGIA
+FROM NHANVIEN
+INNER JOIN HOADON 
+ON NHANVIEN.MANV = HOADON.MANV
+WHERE NHANVIEN.HOTEN='Nguyen Van B' AND NGHD='28/10/2006'
+
+--10. In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+SELECT MASP,TENSP
+FROM KHACHHANG
+INNER JOIN CTHD
+ON 
+WHERE KHACHHANG.HOTEN='Nguyen Van A' AND MONTH(NGHD) =10 AND YEAR(NGHD)=2006
+--11. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+SELECT SOHD
+FROM CTHD
+WHERE MASP IN ('BB01','BB02')
+--12. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM CTHD
+ WHERE MASP IN('BB01','BB02')  AND SL>=10 AND SL<=20
+--13. Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM CTHD
+ WHERE (MASP='BB01' AND MASP='BB02') AND (SL>=10 AND SL<=20)
+
+--14. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.
+--15. In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+--16. In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006.
+--17. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán được trong năm 2006.
+--18. Tìm số hóa đơn đã mua tất cả các sản phẩm do Singapore sản xuất
+
+
+-- hết I 11 trở lên trigger  --
+-- II  hết --
+-- III  --
